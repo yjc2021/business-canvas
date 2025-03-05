3aa5e1fbe54cde71e7336ec2434b5d7080f40f05
yjc2021 <yongjae.choi20@gmail.com>
2025-03-05 18:58:36 +0900

    chore: shadcn 셋업

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 /// <reference types="vitest/config" />
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   test: {},
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
