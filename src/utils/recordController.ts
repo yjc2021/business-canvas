f1b9bdc83c5a38f7d451c1edbe20aad007d07245
yjc2021 <yongjae.choi20@gmail.com>
2025-03-05 10:54:37 +0900

    fix: RecordController.filter 로직 수정

--- src/utils/recordController.ts
+++ src/utils/recordController.ts
@@ -1,7 +1,9 @@
 import { TRecord } from "../types";
 
 type TRecordWithoutId = Omit<TRecord, "id">;
-
+type TRecordMap = {
+  [K in keyof TRecordWithoutId]: TRecordWithoutId[K][];
+};
 export const RecordController = (records: TRecord[]) => ({
   add: (record: TRecord) => {
     return RecordController([...records, record]);
@@ -12,14 +14,12 @@ export const RecordController = (records: TRecord[]) => ({
   update: (id: number, record: TRecordWithoutId) => {
     return RecordController(records.map((r) => (r.id === id ? { ...record, id } : r)));
   },
-  filter: (filter: Partial<TRecordWithoutId>) => {
-    const filterEntries = Object.entries(filter);
-    // TODO: filter 기능 수정
-    // 각 filterEntry는 값이 아니라 **배열** 이다
+  filter: (filter: Partial<TRecordMap>) => {
+    const filterFields = Object.entries(filter);
     return RecordController(
       records.filter((record) => {
-        return filterEntries.every(
-          ([key, value]) => record[key as keyof TRecordWithoutId] === value,
+        return filterFields.every(([field, values]) =>
+          values.some((value) => record[field as keyof TRecordWithoutId] === value),
         );
       }),
     );
